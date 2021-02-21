f9438d648e1092f1d9b31eef7b464f352c4a0078
Towhidul Islam <towhidulrafi1999@gmail.com>
2021-02-22 01:06:10 +0600

    last

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log(23)
+//localStorage.setItem("name" , "rafi")
+//sessionStorage.setItem("name" , "rafi")  
+// const nam = sessionStorage.getItem("name")
+// console.log(nam)
+
+const person= {
+    name : "rafi",
+    age : 23,
+    id: 34,
+    institute : "DU",
+    salary : 0
+}
+
+localStorage.setItem("person" ,JSON.stringify(person))
+const personData = localStorage.getItem("person");
+console.log(personData)
+const personParsed = JSON.parse(personData)
+console.log(personParsed)
+console.log(personParsed.name)
+
+console.log(Object.keys(personParsed))
+console.log(Object.values(personParsed))
